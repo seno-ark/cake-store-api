41a53131747a4a7b75e104993fd1c84c551e9d8c
seno <15000859+seno-ark@users.noreply.github.com>
2022-03-26 21:14:27 +0700

    completing RepositoryMock methods

--- repository/cake_mock.go
+++ repository/cake_mock.go
@@ -5,18 +5,37 @@ import (
 	"cake-store-api/model"
 )
 
-func (r *RepositoryMock) CountCake() (int, error) {
-	return 0, nil
+func (r *RepositoryMock) CountCake() (count int, err error) {
+
+	args := r.Mock.Called()
+
+	if args.Get(0) != nil {
+		count = args.Get(0).(int)
+	}
+
+	if args.Get(1) != nil {
+		err = args.Get(1).(error)
+	}
+
+	return
 }
 
-func (r *RepositoryMock) GetCakeList(params config.M) ([]*model.CakeModel, error) {
-	return nil, nil
+func (r *RepositoryMock) GetCakeList(params config.M) (cakes []*model.CakeModel, err error) {
+
+	args := r.Mock.Called(params)
+
+	if args.Get(0) != nil {
+		cakes = args.Get(0).([]*model.CakeModel)
+	}
+
+	if args.Get(1) != nil {
+		err = args.Get(1).(error)
+	}
+
+	return
 }
 
-func (r *RepositoryMock) GetCake(cakeID int) (*model.CakeModel, error) {
-
-	var cake *model.CakeModel
-	var err error
+func (r *RepositoryMock) GetCake(cakeID int) (cake *model.CakeModel, err error) {
 
 	args := r.Mock.Called(cakeID)
 
@@ -28,17 +47,46 @@ func (r *RepositoryMock) GetCake(cakeID int) (*model.CakeModel, error) {
 		err = args.Get(1).(error)
 	}
 
-	return cake, err
+	return
 }
 
-func (r *RepositoryMock) CreateCake(cakeForm *model.CakeForm) (int, error) {
-	return 0, nil
+func (r *RepositoryMock) CreateCake(cakeForm *model.CakeForm) (cakeID int, err error) {
+
+	args := r.Mock.Called(cakeForm)
+
+	if args.Get(0) != nil {
+		cakeID = args.Get(0).(int)
+	}
+
+	if args.Get(1) != nil {
+		err = args.Get(1).(error)
+	}
+
+	return
 }
 
-func (r *RepositoryMock) UpdateCake(cakeID int, cakeForm *model.CakeForm) (int, error) {
-	return 0, nil
+func (r *RepositoryMock) UpdateCake(id int, cakeForm *model.CakeForm) (cakeID int, err error) {
+
+	args := r.Mock.Called(id, cakeForm)
+
+	if args.Get(0) != nil {
+		cakeID = args.Get(0).(int)
+	}
+
+	if args.Get(1) != nil {
+		err = args.Get(1).(error)
+	}
+
+	return
 }
 
-func (r *RepositoryMock) DeleteCake(cakeID int) error {
-	return nil
+func (r *RepositoryMock) DeleteCake(cakeID int) (err error) {
+
+	args := r.Mock.Called(cakeID)
+
+	if args.Get(0) != nil {
+		err = args.Get(0).(error)
+	}
+
+	return
 }
